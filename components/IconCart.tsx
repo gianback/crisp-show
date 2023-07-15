7397d5b639d57064c635bad7c620842997d75470
Gian Franco <gianfraback@gmail.com>
2023-07-15 00:46:53 -0500

    fix: clip-rule to clipRule

--- components/IconCart.tsx
+++ components/IconCart.tsx
@@ -9,13 +9,13 @@ export function IconCart() {
     >
       <path
         fillRule="evenodd"
-        clip-rule="evenodd"
+        clipRule="evenodd"
         d="M3.26297 6.59995L2.54533 18.8H15.4547L14.737 6.59995H3.26297ZM17 4.19995H1L0 21.2H18L17 4.19995Z"
         fill="white"
       />
       <path
         fillRule="evenodd"
-        clip-rule="evenodd"
+        clipRule="evenodd"
         d="M6.20078 2.4V4.2H3.80078V2.2C3.80078 0.984973 4.78576 0 6.00078 0H12.0008C13.2158 0 14.2008 0.984974 14.2008 2.2V4.2H11.8008V2.4H6.20078Z"
         fill="white"
       />
