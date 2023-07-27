2207f554f27956ca71e1e278b64bcd179d3d4099
Gian Franco <gianfraback@gmail.com>
2023-07-27 01:58:48 -0500

    refactor: navbar mobile padding was added

--- components/Nabvar.tsx
+++ components/Nabvar.tsx
@@ -30,11 +30,11 @@ export function Nabvar() {
   return (
     <>
       <nav
-        className={`fixed left-0 right-0 bg-primary text-white transition-top duration-500 ease-in-out lg:bg-black  lg:left-1/2 lg:translate-x-[-50%] lg:top-[50%] lg:absolute lg:translate-y-[-50%]  ${
+        className={`fixed left-0 right-0 bg-primary pt-24 pb-10 px-4 lg:p-0 text-white transition-top duration-500 ease-in-out lg:bg-black  lg:left-1/2 lg:translate-x-[-50%] lg:top-[50%] lg:absolute lg:translate-y-[-50%]  ${
           isMenuActive ? 'top-0' : 'top-[-100%]'
         }`}
       >
-        <ul className="lg:flex lg:items-center lg:justify-evenly">
+        <ul className="grid gap-4 lg:flex lg:items-center lg:justify-evenly">
           {menuList.map(({ id, label, url }) => (
             <li key={id}>
               <Link href={url}>{label}</Link>
