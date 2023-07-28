bb0d1e0e6c566cb2369f9ff1885acbd5b3838e97
Gian Franco <gianfraback@gmail.com>
2023-07-27 21:38:01 -0500

    feat: offers section in progress

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { MainBanner } from '@/components/Home/MainBanner';
+import { Offers } from '@/components/Home/Offers';
 import 'swiper/css';
 export default function Home() {
   return (
     <main>
       <MainBanner />
+      <Offers />
     </main>
   );
 }
---
+++ components/Home/Offers.tsx
@@ -0,0 +1,14 @@
+
+export function Offers() {
+  return (
+    <section className="grid grid-cols-2">
+        <div className="flex flex-col">
+            <img src="/power-fontain/thermaltake-700w.jpg" className="w-full h-full object-cover" alt="" />
+
+        </div>
+        <picture>
+            <img src="/power-fontain/cooler-master-750w.jpg" className="w-full h-full object-cover" alt="" />
+        </picture>
+    </section>
+  )
+}
