9615320259746298d3139a5ff4f921edd07b890b
Gian Franco <gianfraback@gmail.com>
2023-07-28 15:10:16 -0500

    fix: navbarprovider wrap correctly

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,33 +1,33 @@
 'use client';
 import logo from '@/public/logo.png';
 import { NavbarProvider } from '@/context/NavbarContext';
+import { Container } from './Container';
 import { Nabvar } from './Nabvar';
 import { IconSearch } from './IconSearch';
 import { IconCart } from './IconCart';
-import { Container } from './Container';
+import { Overlay } from './Overlay';
 
 export function Header() {
   return (
-    <header className="sticky top-0 bg-black">
-      <NavbarProvider>
+    <NavbarProvider>
+      <header className="sticky top-0 bg-black z-10">
         <Container className="flex justify-between items-center relative">
           <Nabvar />
-          <picture>
+          <picture className="z-[20]">
             <img
               src={logo.src}
               width={150}
               height={150}
               alt="Logo Crisp-shop"
-              className="z-[20]"
             />
           </picture>
           <div className="flex gap-4">
             <IconSearch />
             <IconCart />
           </div>
-          <div className="min-h-screen w-full fixed inset-0 opacity-70 z-[-1]"></div>
         </Container>
-      </NavbarProvider>
-    </header>
+      </header>
+      <Overlay />
+    </NavbarProvider>
   );
 }
