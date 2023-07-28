f81c248c72b2fbcec87bff8b88caee6970bfd73c
Gian Franco <gianfraback@gmail.com>
2023-07-28 15:10:54 -0500

    feat: add interface to navbarcontext

--- context/NavbarContext.tsx
+++ context/NavbarContext.tsx
@@ -1,28 +1,33 @@
-import { ReactNode, createContext, useContext, useState } from "react";
+import { ReactNode, createContext, useContext, useState } from 'react';
 
-interface Props{
-    children: ReactNode
+interface Props {
+  children: ReactNode;
 }
 
-export const NavbarContext = createContext({
-    isMenuActive:false,
-    setMenuActive: (_value:boolean) => {}
-})
-
-export const NavbarProvider = ({children}:Props) =>{
-    const [isMenuActive, setMenuActive] = useState(false)
-
-    return(
-        <NavbarContext.Provider value={{isMenuActive,setMenuActive}}>
-        {children}
-        </NavbarContext.Provider>
-    )
+interface NavbarContextType {
+  isMenuActive: boolean;
+  setMenuActive: (newValue: boolean) => void;
 }
 
-export const useNavbarContext = () =>{
-    const context = useContext(NavbarContext)
-    if(context === undefined){
-        throw new Error('useNavbarProvider must be used within a NavbarProvider')
-    }
-    return context
-}
+export const NavbarContext = createContext<NavbarContextType>({
+  isMenuActive: false,
+  setMenuActive: () => {},
+});
+
+export const NavbarProvider = ({ children }: Props) => {
+  const [isMenuActive, setMenuActive] = useState(false);
+
+  return (
+    <NavbarContext.Provider value={{ isMenuActive, setMenuActive }}>
+      {children}
+    </NavbarContext.Provider>
+  );
+};
+
+export const useNavbarContext = () => {
+  const context = useContext(NavbarContext);
+  if (context === undefined) {
+    throw new Error('useNavbarProvider must be used within a NavbarProvider');
+  }
+  return context;
+};
