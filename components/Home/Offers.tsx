d420da08c8aa9d2b5100163a3bbd5b5b49d6c3d7
Gian Franco <gianfraback@gmail.com>
2023-07-28 15:10:28 -0500

    feat: add offers to home

--- components/Home/Offers.tsx
+++ components/Home/Offers.tsx
@@ -1,14 +1,23 @@
-
 export function Offers() {
   return (
     <section className="grid grid-cols-2">
-        <div className="flex flex-col">
-            <img src="/power-fontain/thermaltake-700w.jpg" className="w-full h-full object-cover" alt="" />
-
-        </div>
-        <picture>
-            <img src="/power-fontain/cooler-master-750w.jpg" className="w-full h-full object-cover" alt="" />
-        </picture>
+      <div className="grid gap-1 place-items-center">
+        <img
+          src="/power-fontain/thermaltake-700w.jpg"
+          alt=""
+        />
+        <img
+          src="/monitors/zowie_rl2755.jpg"
+          alt=""
+        />
+      </div>
+      <picture>
+        <img
+          src="/power-fontain/cooler-master-750w.jpg"
+          className="w-full object-cover"
+          alt=""
+        />
+      </picture>
     </section>
-  )
+  );
 }
