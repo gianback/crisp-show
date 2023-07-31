127eab79d178169d31f3dab96ded3f84d6d18bb3
Gian Franco <gianfraback@gmail.com>
2023-07-31 00:27:15 -0500

    feat: home add offers and banner offers components

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
+import { BannerOffer } from '@/components/Home/BannerOffer';
 import { MainBanner } from '@/components/Home/MainBanner';
 import { Offers } from '@/components/Home/Offers';
 import 'swiper/css';
-export default function Home() {
+export default async function Home() {
+  const resp = await fetch(
+    'http://localhost:3000/api/category/a655d924-ae82-4597-8219-e106ae20e8a2',
+  );
+  const data = await resp.json();
   return (
     <main>
       <MainBanner />
-      <Offers />
+      <Offers offers={data} />
+      <BannerOffer />
     </main>
   );
 }
