590d4dc93363c43f738b01eeb7890d5494c79cd6
Gian Franco <gianfraback@gmail.com>
2023-07-31 00:24:08 -0500

    refactor: switch const for let

--- services/cloudinary.service.ts
+++ services/cloudinary.service.ts
@@ -1,22 +1,22 @@
-import { v2 as cloudinary } from "cloudinary";
-import { Readable } from "stream";
+import { v2 as cloudinary } from 'cloudinary';
+import { Readable } from 'stream';
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME ?? "",
-  api_key: process.env.CLOUDINARY_PUBLIC_API_KEY ?? "",
-  api_secret: process.env.CLOUDINARY_SECRET_KEY ?? "",
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME ?? '',
+  api_key: process.env.CLOUDINARY_PUBLIC_API_KEY ?? '',
+  api_secret: process.env.CLOUDINARY_SECRET_KEY ?? '',
 });
 
 export const cloudinaryService = async (image: Buffer) => {
   return new Promise((res, rej) => {
     const theTransformStream = cloudinary.uploader.upload_stream(
-      { resource_type: "image" },
+      { resource_type: 'image' },
       (err, result) => {
         if (err) return rej(err);
         res(result);
-      }
+      },
     );
-    let str = Readable.from(image);
+    const str = Readable.from(image);
     str.pipe(theTransformStream);
   });
 };
