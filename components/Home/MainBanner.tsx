e68ab800e30ede6cc1fbe572bf05aad13d426493
Gian Franco <gianfraback@gmail.com>
2023-07-31 00:27:28 -0500

    feat: add container component

--- components/Home/MainBanner.tsx
+++ components/Home/MainBanner.tsx
@@ -3,6 +3,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import { SwiperOptions } from 'swiper/types';
 import { Autoplay, Navigation } from 'swiper/modules';
 import { IconArrowLeft } from '../IconArrowLeft';
+import { Container } from '../Container';
 const bannerList = [
   {
     id: 1,
@@ -31,26 +32,28 @@ const swiperOptions: SwiperOptions = {
 
 export function MainBanner() {
   return (
-    <section className="relative  [&>button]:absolute [&>button]:top-1/2 [&>button]:translate-y-[-50%] [&>button]:z-[5]">
-      <Swiper {...swiperOptions}>
-        {bannerList.map(({ id, img }) => (
-          <SwiperSlide key={id}>
-            <picture>
-              <img
-                src={img}
-                alt="image banner"
-                className="w-full h-full"
-              />
-            </picture>
-          </SwiperSlide>
-        ))}
-      </Swiper>
-      <button className="w-8 lg:w-14 flex items-center justify-center py-1 px-2 lg:py-2 lg:px-3 prev-btn left-4 bg-primary rotate-180 rounded-md text-white">
-        <IconArrowLeft />
-      </button>
-      <button className="w-8 lg:w-14 flex items-center justify-center py-1 px-2 lg:py-2 lg:px-3 next-btn right-4 bg-primary rounded-md  text-white">
-        <IconArrowLeft />
-      </button>
+    <section>
+      <Container className="relative  [&>button]:absolute [&>button]:top-1/2 [&>button]:translate-y-[-50%] [&>button]:z-[5]">
+        <Swiper {...swiperOptions}>
+          {bannerList.map(({ id, img }) => (
+            <SwiperSlide key={id}>
+              <picture>
+                <img
+                  src={img}
+                  alt="image banner"
+                  className="w-full h-full"
+                />
+              </picture>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+        <button className="w-8 lg:w-14 flex items-center justify-center py-1 px-2 lg:py-2 lg:px-3 prev-btn left-4 bg-primary rotate-180 rounded-md text-white">
+          <IconArrowLeft />
+        </button>
+        <button className="w-8 lg:w-14 flex items-center justify-center py-1 px-2 lg:py-2 lg:px-3 next-btn right-4 bg-primary rounded-md  text-white">
+          <IconArrowLeft />
+        </button>
+      </Container>
     </section>
   );
 }
