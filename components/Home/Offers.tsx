8bc87435f72948d384128c17f699db546be676a6
Gian Franco <gianfraback@gmail.com>
2023-07-31 00:26:00 -0500

    feat: add data from api

--- components/Home/Offers.tsx
+++ components/Home/Offers.tsx
@@ -1,23 +1,31 @@
-export function Offers() {
+import { Product } from '@/interfaces/product';
+import Link from 'next/link';
+import { Container } from '../Container';
+
+type OffersType = {
+  offers: Product[];
+};
+
+export function Offers({ offers }: OffersType) {
   return (
-    <section className="grid grid-cols-2">
-      <div className="grid gap-1 place-items-center">
-        <img
-          src="/power-fontain/thermaltake-700w.jpg"
-          alt=""
-        />
-        <img
-          src="/monitors/zowie_rl2755.jpg"
-          alt=""
-        />
-      </div>
-      <picture>
-        <img
-          src="/power-fontain/cooler-master-750w.jpg"
-          className="w-full object-cover"
-          alt=""
-        />
-      </picture>
+    <section>
+      <Container>
+        <div className="grid grid-cols-2">
+          {offers.map(({ name, mainPicture, id }) => (
+            <Link
+              key={id}
+              href={`/product/${id}`}
+            >
+              <picture>
+                <img
+                  src={mainPicture}
+                  alt={name}
+                />
+              </picture>
+            </Link>
+          ))}
+        </div>
+      </Container>
     </section>
   );
 }
