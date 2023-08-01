bc92c6cd98722480ceaf00ba52d94f9bfb469249
Gian Franco <gianfraback@gmail.com>
2023-08-01 02:22:08 -0500

    feat: add shop page

---
+++ app/shop/page.tsx
@@ -0,0 +1,35 @@
+'use client';
+import { Container } from '@/components/Container';
+import { useState } from 'react';
+import { DropDawn } from './product/DropDawn';
+
+export default function Store() {
+  const [menuFilerActive, setMenuFilerActive] = useState(false);
+
+  const handleMenuFilter = () => {
+    setMenuFilerActive(!menuFilerActive);
+  };
+  return (
+    <main>
+      <Container>
+        <div
+          className="border border-solid border-black bg-white py-2 px-3 relative"
+          onClick={handleMenuFilter}
+        >
+          <span className="uppercase">Filtrar</span>
+
+          <div
+            className={`fixed bg-white left-0 w-full  transition-all ease-in duration-75 ${
+              menuFilerActive ? 'top-30' : '-top-[100%]'
+            }`}
+          >
+            filtros y mas filtros
+            <DropDawn title="marca">
+              <p>filtros de marca</p>
+            </DropDawn>
+          </div>
+        </div>
+      </Container>
+    </main>
+  );
+}
